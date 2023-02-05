47b1fafba63774abce78679f3e297404022e5604
Naga-Tharun <nagatharunmakkena799@gmail.com>
2023-02-05 20:26:40 +0530

    data files qn2, qn4

--- lab-1/qn2/segregate.py
+++ lab-1/qn2/segregate.py
@@ -4,30 +4,37 @@ os.system("mkdir plot_data")
 
 os.system("mkdir plot_data/RMT")
 # Enable only for first time, then disable
-os.system("mkdir plot_data/RMT/thread_1")
-for i in range(2, 17, 2):
-  cmd = "mkdir plot_data/RMT/thread_" + str(i)
+for i in [4, 8, 16, 32, 64]:
+  cmd = "mkdir plot_data/RMT/block_" + str(i)
   os.system(cmd)
+  cmd = "mkdir plot_data/RMT/block_" + str(i) + "/thread_1"
+  os.system(cmd)
+  for j in range(2, 17, 2):
+    cmd = "mkdir plot_data/RMT/block_" + str(i) + "/thread_" + str(j)
+    os.system(cmd)
 
 with open("dump.dat") as f:
   for line in f:
     tmp = line.split("\n")[0]
     tp = tmp.split("\t")
 
-    cmd = "echo \"" + tmp + "\" >> plot_data/RMT/thread_" + tp[2] + "/RMT_" + tp[1] + ".dat"
+    cmd = "echo \"" + tmp + "\" >> plot_data/RMT/block_" + tp[3] + "/thread_" + tp[2] + "/RMT_" + tp[1] + ".dat"
     os.system(cmd)
 
 
 os.system("mkdir plot_data/RTM")
 # Enable only for first time, then disable
-os.system("mkdir plot_data/RTM/matrix_512")
-os.system("mkdir plot_data/RTM/matrix_1024")
-os.system("mkdir plot_data/RTM/matrix_2048")
+for i in [4, 8, 16, 32, 64]:
+  cmd = "mkdir plot_data/RTM/block_" + str(i)
+  os.system(cmd)
+  for j in [512, 1024, 2048]:
+    cmd = "mkdir plot_data/RTM/block_" + str(i) + "/matrix_" + str(j)
+    os.system(cmd)
 
 with open("dump.dat") as f:
   for line in f:
     tmp = line.split("\n")[0]
     tp = tmp.split("\t")
 
-    cmd = "echo \"" + tmp + "\" >> plot_data/RTM/matrix_" + tp[0] + "/RTM_" + tp[1] + ".dat"
+    cmd = "echo \"" + tmp + "\" >> plot_data/RTM/block_" + tp[3] + "/matrix_" + tp[0] + "/RTM_" + tp[1] + ".dat"
     os.system(cmd)
